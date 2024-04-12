7bc4a92c48ea78a0a596ff0a99ac535920e9aa88
David <okidoaki@hotmail.fr>
2024-04-12 11:58:10 +0200

    Added the serum.h file
    To include in you projects

--- src/serum-decode.h
+++ src/serum-decode.h
@@ -8,54 +8,27 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-typedef unsigned char UINT8;
-typedef unsigned short UINT16;
-typedef unsigned int UINT32;
-typedef unsigned int UINT;
+#include "serum.h"
 
-const int MAX_DYNA_4COLS_PER_FRAME =
-    16;  // max number of color sets for dynamic content for each frame
-const int MAX_SPRITE_SIZE = 128;  // maximum size of the sprites
-const int MAX_SPRITES_PER_FRAME =
-    32;  // maximum amount of sprites to look for per frame
-const int MAX_COLOR_ROTATIONS =
-    8;  // maximum amount of color rotations per frame
-const int MAX_SPRITE_DETECT_AREAS =
-    4;  // maximum number of areas to detect the sprite
-
-const int PALETTE_SIZE = 64 * 3;  // size of a palette
-const int ROTATION_SIZE =
-    3 * MAX_COLOR_ROTATIONS;  // size of a color rotation block
-const int MAX_SPRITE_TO_DETECT =
-    16;  // max number of sprites detected in a frame
-const int MAX_BACKGROUND_IMAGES = 255;  // max number of background images
-
-SERUM_API bool Serum_LoadFile(const char* const filename, int* pwidth,
-                              int* pheight, unsigned int* pnocolors,
-                              unsigned int* pntriggers);
-SERUM_API bool Serum_Load(const char* const altcolorpath,
-                          const char* const romname, int* pwidth, int* pheight,
-                          unsigned int* pnocolors, unsigned int* pntriggers);
+SERUM_API bool Serum_LoadFile(const char* const filename, unsigned int* pnocolors, unsigned int* pntriggers, UINT8 flags, UINT* width32, UINT* width64);
+SERUM_API bool Serum_Load(const char* const altcolorpath, const char* const romname, unsigned int* pnocolors, unsigned int* pntriggers, UINT8 flags, UINT* width32, UINT* width64, UINT8* newformat);
 SERUM_API void Serum_SetIgnoreUnknownFramesTimeout(UINT16 milliseconds);
 SERUM_API void Serum_SetMaximumUnknownFramesToSkip(UINT8 maximum);
-SERUM_API void Serum_SetStandardPalette(const UINT8* palette,
-                                        const int bitDepth);
+SERUM_API void Serum_SetStandardPalette(const UINT8* palette, const int bitDepth);
 SERUM_API void Serum_Dispose(void);
-SERUM_API bool Serum_ColorizeWithMetadata(UINT8* frame, int width, int height,
-                                          UINT8* palette, UINT8* rotations,
-                                          UINT32* triggerID, UINT32* hashcode,
-                                          int* frameID);
-SERUM_API bool Serum_Colorize(UINT8* frame, int width, int height,
-                              UINT8* palette, UINT8* rotations,
-                              UINT32* triggerID);
-SERUM_API bool Serum_ApplyRotations(UINT8* palette, UINT8* rotations);
-SERUM_API bool Serum_ColorizeWithMetadataOrApplyRotations(
-    UINT8* frame, int width, int height, UINT8* palette, UINT8* rotations,
-    UINT32* triggerID, UINT32* hashcode, int* frameID);
+SERUM_API bool Serum_ColorizeWithMetadata(UINT8* frame, Serum_Frame* poldframe);
+SERUM_API bool Serum_ColorizeWithMetadataN(UINT8* frame, Serum_Frame_New* pnewframe);
+SERUM_API bool Serum_Colorize(UINT8* frame, Serum_Frame* poldframe, Serum_Frame_New* pnewframe);
+SERUM_API bool Serum_ApplyRotations(Serum_Frame* poldframe);// UINT8* palette, UINT8* rotations);
+SERUM_API bool Serum_ApplyRotationsN(Serum_Frame_New* pnewframe, UINT8* modelements32, UINT8* modelements64);// UINT16* frame, UINT8* modelements, UINT16* rotationsinframe, UINT sizeframe, UINT16* rotations, bool is32);
+/*SERUM_API bool Serum_ColorizeWithMetadataOrApplyRotations(
+	UINT8* frame, int width, int height, UINT8* palette, UINT8* rotations,
+	UINT32* triggerID, UINT32* hashcode, int* frameID);
 SERUM_API bool Serum_ColorizeOrApplyRotations(UINT8* frame, int width,
-                                              int height, UINT8* palette,
-                                              UINT32* triggerID);
+											  int height, UINT8* palette,
+											  UINT32* triggerID);*/
 SERUM_API void Serum_DisableColorization(void);
 SERUM_API void Serum_EnableColorization(void);
 SERUM_API const char* Serum_GetVersion(void);
 SERUM_API const char* Serum_GetMinorVersion(void);
+
